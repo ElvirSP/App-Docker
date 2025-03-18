cf57b85fc0fb570c7f4b183e955fb9d643711669
Elvir <esandovalp5@miumg.edu.gt>
2025-03-17 20:07:50 -0600

    Cambio para video

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Ruta principal
-app.get("/", (req, res) => {
-    res.send("¡Hola desde Render con Docker! 🚀");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = [];
+
+app.get("/items", (req, res) => {
+    res.json(items);
 });
 
-// Iniciar servidor
+app.post("/add", (req, res) => {
+    const { item } = req.body;
+    if (item) {
+        items.push(item);
+    }
+    res.sendStatus(200);
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
